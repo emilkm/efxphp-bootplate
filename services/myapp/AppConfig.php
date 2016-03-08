14a0a9fee3eac649bf540fcc8f707ab62c4a673f
emilkm <emilkm@@users.noreply.github.com>
2016-03-08 22:28:26 +1300

    Add sample property to AppConfig

--- services/myapp/AppConfig.php
+++ services/myapp/AppConfig.php
@@ -3,10 +3,22 @@ namespace myapp\server;
 
 use emilkm\efxphp\ServerConfig;
 
+/**
+ * List properties for intellisense in some IDEs
+ *
+ * @property array $databases
+ */
 class AppConfig extends ServerConfig
 {
     public function __construct()
     {
         $this->productionMode = false;
     }
+    
+    /**
+     * Array of database connection settings
+     * 
+     * @var array
+     */
+    protected $databases;
 }
